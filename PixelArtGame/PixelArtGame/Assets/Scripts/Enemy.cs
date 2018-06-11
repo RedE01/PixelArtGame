38a01852507c067362ce25aa4da9a7c368adcb3e
RedE01 <e.sternvik@gmail.com>
2018-06-11 22:13:19 +0200

    Added character
    Added character: Movement, attack particles.

---
+++ PixelArtGame/PixelArtGame/Assets/Scripts/Enemy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	GameObject target;
+	Rigidbody2D rb;
+
+	void Start() {
+		target = GameObject.FindGameObjectWithTag("Player");
+		rb = GetComponent<Rigidbody2D>();
+	}
+
+}
---
+++ PixelArtGame/PixelArtGame/Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed;
+
+	Vector2 movement;
+	Rigidbody2D rb;
+	GameObject attackParticle;
+	ParticleSystem attackParticleSystem;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		attackParticle = GameObject.Find("AttackParticle");
+		attackParticleSystem = attackParticle.GetComponent<ParticleSystem>();
+	}
+
+	void Update() {
+		Movement();
+		Attack();
+		
+	}
+
+	void FixedUpdate() {
+		rb.MovePosition(movement);
+	}
+
+	void Movement() {
+		movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+		if (movement.magnitude > 1) {
+			movement = movement.normalized;
+		}
+		movement *= speed / 100;
+		movement.x += transform.position.x;
+		movement.y += transform.position.y;
+	}
+
+	void Attack() {
+		if (Input.GetKeyDown(KeyCode.Mouse0)) {
+			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			Vector2 partDir = new Vector2(mousePos.x - transform.position.x, mousePos.y - transform.position.y).normalized;
+
+			Vector2 partPos = new Vector2(partDir.x + transform.position.x, partDir.y + transform.position.y);
+			attackParticle.transform.position = partPos;
+
+			var partMain = attackParticleSystem.main;
+			partMain.startRotation = Mathf.Atan2(partDir.x, partDir.y) - 2.25f;
+
+			attackParticleSystem.Play();
+		}
+	}
+}
